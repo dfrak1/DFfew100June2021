857cf137f46b157d95c23a37f9e86cd19506028e
dfrak1 <83408622+dfrak1@users.noreply.github.com>
2021-06-02 20:15:14 +0000

    Update

--- src/app/specs/01_types.sepc.ts
+++ src/app/specs/01_types.sepc.ts
@@ -1,3 +1,5 @@
+import { stringify } from "@angular/compiler/src/util";
+
 describe('types in typescript', () => {
 
     describe('variables and constraints in typescript', () => {
@@ -16,9 +18,45 @@ describe('types in typescript', () => {
             x = 3.14;
             x = 12;
             x = 121_456_789;
-            x = 'tacos';
+            // x = 'tacos';
+
+        });
+        it('union types in typescript', () => {
+            let x: number | string;
+
+            x = 'dog';
+
+            x = 12;
+        });
+        it('intializing variable with let', () => {
+            let name = 'Joe';
+
+            let age: string | number = 'old';
+
+            age = 42;
 
         });
     });
+    describe('using const', () => {
+        it('const allows you to declare a name for a vlue that cannot be reassinged', () => {
+            const PI = 3.14; //const must be intialized and cannot be redefined
+            //PI = 22;
+            const favoriteNumbers = [9, 20, 108];
+            //favoriteNumbers =[2,4];
+            favoriteNumbers[0] = 2;
+            const movie = {
+                title: 'Thor',
+                director: 'Taika Waititi',
+                yearReleased: 2022
+            };
+            movie.title = 'Thor: Love and Thunder';
+        });
 
+        describe('details of some data types', () => {
+
+
+        });
+
+
+    });
 });
